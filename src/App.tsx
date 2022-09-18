31c5d50f293be2459b8396c831d8b6bebbe46bc0
Vladislav <vladozdeveloper@gmail.com>
2022-09-18 23:45:34 +0300

    added content and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='conatiner'>
+      <h1 className='font-bold'>Hello</h1>
+      <button>Click me!</button>
+    </div>
+  );
+}
+
+export default App;
