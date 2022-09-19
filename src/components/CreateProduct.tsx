d6d018f0e9e8caf4908a054da1d25676d1668779
Vladislav <vladozdeveloper@gmail.com>
2022-09-19 13:03:33 +0300

    created modal and createnewproduct

---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+import {IProduct} from '../models'
+import axios from 'axios'
+import {ErrorMessage} from './ErrorMessage'
+
+const productData: IProduct =  {
+  title: '',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    rate: 42,
+    count: 10
+  }
+}
+
+export function CreateProduct() {
+  const [value, setValue] = useState('')
+  const [error, setError] = useState('')
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setError('')
+
+    if (value.trim().length === 0) {
+      setError('Please enter valid title.')
+      return
+    }
+
+    productData.title = value;
+    await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+
+  }
+
+  const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-0"
+        placeholder="Enter product title..."
+        value={value}
+        onChange={changeHandler}
+      />
+
+      {error ? <ErrorMessage error={error} />: null}
+
+      <button type="submit" className="py-2 px-4 border bg-yellow-400 hover:text-white">Create</button>
+    </form>
+  )
+}
---
+++ src/hooks/products.ts
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react'
+import {IProduct} from '../models'
+import axios from 'axios'
+
+export function useProducts() {
+  const [products,setProducts]=useState<IProduct[]>([]);
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState();
+
+  async function fetchProducts() {
+    
+    setLoading(true);
+    await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+      .then (response=>{setProducts(response.data)})
+      .then (()=>{setLoading(false)})
+      .catch (error=>{
+        setError(error.message); 
+        setLoading(false);
+      })
+  }
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+  return { products, error, loading }
+}
